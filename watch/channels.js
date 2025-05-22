260780f00ad73cf679ec12e125120e0e9f2db357
themirrazz <89490659+themirrazz@users.noreply.github.com>
2025-05-22 14:06:49 -0700

    Add ESPNU and Disney Channel

--- watch/channels.js
+++ watch/channels.js
@@ -1,17 +1,38 @@
 const CHANNELS = [
     {
-        id: 'abc-news',
         name: "ABC News",
         feed: "https://content.uplynk.com/channel/3324f2467c414329b3b0cc5cd987b6be.m3u8",
-        category: 'new',
-        hd: true,
+        category: 'news',
+        quality: 'hd',
         schedule: {
             available: false
         }
     },
     {
-        id: 'bloomberg',
         name: "Bloomberg Quicktake",
-        feed: "https://bloomberg-quicktake-1-be.samsung.wurl.tv/playlist.m3u8"
+        feed: "https://bloomberg-quicktake-1-be.samsung.wurl.tv/playlist.m3u8",
+        category: 'finance',
+        quality: 'hd',
+        schedule: {
+            available: false
+        }
+    },
+    {
+        name: "ESPNU",
+        feed: "https://fl3.moveonjoy.com/ESPN_U/index.m3u8",
+        category: 'sports',
+        quality: 'hd',
+        schedule: {
+            available: false
+        }
+    },
+    {
+        name: "Disney Channel",
+        feed: "https://fl5.moveonjoy.com/DISNEY/index.m3u8",
+        category: 'kids',
+        quality: 'hd',
+        schedule: {
+            available: false
+        }
     }
 ];
