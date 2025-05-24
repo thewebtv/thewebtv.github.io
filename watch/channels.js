0526b8f0b8ec3facc56bf3ec7029ebd4d7bd235d
themirrazz <89490659+themirrazz@users.noreply.github.com>
2025-05-23 17:16:10 -0700

    add more channels

--- watch/channels.js
+++ watch/channels.js
@@ -20,6 +20,16 @@ const CHANNELS = [
         }
     },
     {
+        id: 'nbc-now',
+        name: "NBC News NOW",
+        feed: "https://epg.provider.plex.tv/library/parts/5e20b730f2f8d5003d739db7-5fc70600dd53a6002d8f93ca/variant.m3u8?x-plex-token=pBAhJwbvHDSzsvJyQxTe&url=9cfe799c2b08f5c9e35e6cee404a1a99-566d10681034d59d58fe2d518af741368ca3817c1cd528aeac1c7e1b73cffc5d04883b10afd074ad03956c7aca90c745b5d61442339ca3627a7b8dbb00fc4fe61832f102258e7f5840f46da2a0b7c9bba3b5dd91512027d076acc9455957c84d810ad98065722ce5cd27e17dfbdc7a2b40dc071edd97563be484fced28fc33abdd0243dedcefe21637e54975ceef56ae82b074ca044c83bff0a03e63ce3fef4f",
+        category: 'News',
+        quality: 'hd',
+        schedule: {
+            available: false
+        }
+    },
+    {
         id: 'quicktake',
         name: "Bloomberg Quicktake HD",
         feed: "https://bloomberg-quicktake-1-be.samsung.wurl.tv/playlist.m3u8",
@@ -30,10 +40,49 @@ const CHANNELS = [
         }
     },
     {
+        id: 'nbc-sports-now',
+        name: "NBC Sports NOW",
+        feed: "https://d4whmvwm0rdvi.cloudfront.net/10007/99951253/hls/master.m3u8?ads.xumo_channelId=99951253&ads.asnw=169843&ads.afid=380753925&ads.sfid=19208128",
+        category: 'Sports',
+        quality: 'hd',
+        schedule: {
+            available: false
+        }
+    },
+    {
+        id: 'cbs-sports-hq',
+        name: "CBS Sports HQ",
+        feed: "https://dai.google.com/linear/hls/pa/event/9Lq0ERvoSR-z9AwvFS-xYA/stream/518a7fc2-3dc0-455f-9b31-611710ac9350:CHS/variant/f1394531f46e3ae55a7bfe72b32eddec/bandwidth/5859480.m3u8",
+        category: 'Sports',
+        quality: 'hd',
+        schedule: {
+            available: false
+        }
+    },
+    {
         id: 'ion-mystery',
         name: "ION Mystery",
         feed: "https://scripps-ionmystery-1-us.xumo.wurl.tv/playlist.m3u8",
-        category: 'Entertainment',
+        category: 'Mystery',
+        quality: 'hd',
+        schedule: {
+            available: false
+        }
+    },
+    {
+        id: 'filmrise-true-crime',
+        name: "Filmrise True Crime",
+        feed: "https://live-manifest.production-public.tubi.io/live/04431aed-2da0-46e2-b481-c6b0582ccc03/playlist.m3u8",
+        category: 'Mystery',
+        quality: 'hd',
+        schedule: {
+            available: false
+        }
+    },
+    {
+        id: 'ae-crime-360',
+        name: "A&E Crime 360",
+        feed: "https://live-manifest.production-public.tubi.io/live/8cba5c00-34f4-45da-a73a-1e3d813a0cfb/playlist.m3u8?token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJjb3VudHJ5IjoiVVMiLCJkZXZpY2VfaWQiOiJlYTY2NjBmMC0zNzY2LTRiZTQtOGQ2Yy1iNGM1ZWNjNTFhNTQiLCJleHAiOjE3NDgzMDM5ODksImlzcyI6ImxpdmVfbmV3c19tYW5pZmVzdCIsInBsYXRmb3JtIjoiV0VCIiwicnNzIjoiYXA6Y3JpbWUtMzYwIiwidXNlcl9pZCI6MH0.7OfV5rg9H5fgAT9CpGwl2ELD5zNEFpitPDFFMK53AQSReDe2uK-9oSxFvH9UF3VLmmWa131JPh2do_XE57Mhvw",
         quality: 'hd',
         schedule: {
             available: false
